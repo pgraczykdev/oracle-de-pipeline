5418e606feb64f306d5ed8cf8c2dc3f42c3b3907
pgraczyk <pgraczyk.de@gmail.com>
2026-06-28 18:52:36 +0200

    connection test with ADB via wallet

---
+++ test_connection.py
@@ -0,0 +1,25 @@
+import os
+import oracledb
+from dotenv import load_dotenv
+
+load_dotenv()
+
+connection = oracledb.connect(
+    user=os.environ["ORACLE_USER"],
+    password=os.environ["ORACLE_PASSWORD"],
+    dsn=os.environ["ORACLE_DSN"],
+    config_dir=os.environ["WALLET_DIR"],     
+    wallet_location=os.environ["WALLET_DIR"], 
+    wallet_password=os.environ["WALLET_PASSWORD"],
+)
+
+with connection.cursor() as cur:
+    cur.execute("SELECT 'połączono!' FROM dual")
+    print(cur.fetchone()[0])
+
+    cur.execute("SELECT COUNT(*) FROM sh.sales")
+    print("SH.SALES wierszy:", cur.fetchone()[0])
+
+
+connection.close()
+
